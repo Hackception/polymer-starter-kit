acfa430549531174e54d883e6552333a18af37fb
Michael Stramel <m.stramel89@gmail.com>
2017-07-27 11:26:53 -0500

    Fix: eslint config & plugins

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  extends: ['eslint:recommended', 'google'],
+  env: {
+    'browser': true,
+    'es6': true,
+  },
+  plugins: [
+    'html',
+  ],
+  rules: {
+    'arrow-parens': [2, 'always'],
+     'indent': [2, 2, {
+      SwitchCase: 1,
+      VariableDecorator: 2,
+    }],
+    'prefer-const': 2,
+    'max-len': [2, {
+      code: 120,
+      tabWidth: 2,
+      ignoreUrls: true,
+      ignorePattern: '^goog\.(module|require)',
+    }],
+    'new-cap': ['error', { 'capIsNewExceptions': ['Polymer'], 'capIsNewExceptionPattern': 'Mixin$' }],
+    'no-unused-expressions': [2, {
+      allowShortCircuit: true,
+      allowTernary: false,
+    }],
+    'no-unused-vars': [2, {
+      vars: 'all',
+      args: 'after-used',
+    }],
+
+    // Plugin HTML
+    'html/indent': '+2',
+    'html/report-bad-indent': '2',
+  },
+  globals: {
+    'customElements': false,
+    'hc': false,
+    'Polymer': false
+  },
+};
